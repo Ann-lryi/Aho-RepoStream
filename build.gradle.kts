4c679e00fb918eb70c19fb8fbc7a07df66249c64
Ann-lryi <123809756+Ann-lryi@users.noreply.github.com>
2026-06-07 11:32:18 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.13.2")
-        classpath("com.github.recloudstream:gradle:68db7215ecbb1dc58ad735247ed3daa862fcc1e4")
+        classpath("com.github.recloudstream.gradle:gradle:81b1d424d2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
